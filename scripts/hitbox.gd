6e5b79477e7b0d408815bc5ebd2053129fe7a0f9
RechieKho <the.real.richie.kho@gmail.com>
2023-01-21 13:50:30 +0800

    Make hurtbox pass impulse (knockback) to hitbox.

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -3,14 +3,19 @@ class_name Hitbox
 
 
 signal on_dead
-signal on_hitpoint_changed(new_hitpoint)
+signal on_hit(new_hitpoint, impulse)
 
 export(int, 0, 100) var max_hitpoints := 10
+export(NodePath) var subject_path := "../"
 
+onready var subject := get_node(subject_path) as Node2D
 onready var hitpoints = max_hitpoints setget set_hitpoints
 
 func set_hitpoints(value: int):
 	if hitpoints == value: return
 	hitpoints = clamp(value, 0, max_hitpoints)
-	emit_signal("on_hitpoint_changed", hitpoints)
 	if hitpoints == 0: emit_signal("on_dead")
+
+func hit(amount: int, impulse: Vector2):
+	set_hitpoints(hitpoints - amount)
+	emit_signal("on_hit", hitpoints, impulse)
--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -3,8 +3,14 @@ class_name Hurtbox
 
 
 export(int, 0, 10) var hurtpoints := 5
+export(float, 0, 10000) var impulse_magnitude := 200
+export(NodePath) var subject_path := "../"
+
+onready var subject := get_node(subject_path) as Node2D
 
 func hurt():
 	for hitbox in get_overlapping_areas():
 		if not hitbox is Hitbox: continue
-		hitbox.hitpoints -= hurtpoints
+		var impulse = (hitbox.subject.global_position - subject.global_position) \
+			.normalized() * impulse_magnitude
+		hitbox.hit(hurtpoints, impulse)
