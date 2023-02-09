0d1b15eba9dad4cc8fefe24b692850f3215a24d9
RechieKho <the.real.richie.kho@gmail.com>
2023-02-09 13:54:07 +0800

    Remove debugging code in mana_system.gd. Add type hinting to mana_system.gd.

--- prefabs/player/mana.gd
+++ prefabs/player/mana.gd
@@ -2,13 +2,13 @@ extends Node
 class_name Mana
 
 
+signal on_mana_changed(new_mana)
+
 export(int, 0, 1000) var max_mana := 100
 
 var mana: int = max_mana setget set_mana
 
-signal on_mana_changed(new_mana)
-
-func set_mana(value):
+func set_mana(value: int):
 	if mana == value: return
 	mana = clamp(value, 0, max_mana)
 	emit_signal("on_mana_changed", mana)
