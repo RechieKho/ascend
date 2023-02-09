6cddcdbc226d2edd60bb28286d1b4f25ab9005b5
Baltazar Gomez <btzr.io@gmail.com>
2023-02-09 13:52:41 -0600

    fix mana_system.gd

--- scripts/mana_system.gd
+++ scripts/mana_system.gd
@@ -8,9 +8,6 @@ var mana: int = max_mana setget set_mana
 
 signal on_mana_changed(new_mana)
 
-func _ready():
-	connect("on_mana_changed", self, "test")
-
 func set_mana(value):
 	if mana == value: return
 	mana = clamp(value, 0, max_mana)
